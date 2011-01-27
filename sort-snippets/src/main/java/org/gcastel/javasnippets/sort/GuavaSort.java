325dadc48c8efdc7dc6a1424aa4a129cb0826463
Gerben Castel <gerben.castel@gmail.com>
2011-01-27 15:12:02 +0100

    No need to sort null entries

--- sort-snippets/src/main/java/org/gcastel/javasnippets/sort/GuavaSort.java
+++ sort-snippets/src/main/java/org/gcastel/javasnippets/sort/GuavaSort.java
@@ -17,11 +17,10 @@ public final class GuavaSort
      * @return a copy of toSort, sorted by Index
      */
     public static List<DataToSort> sortedByIndexCopy(List<DataToSort> toSort) {
-      // Ordering definition : sorted by index, nulls first
+      // Ordering definition : sorted by index
       Ordering<DataToSort> byIndexOrdering = new Ordering<DataToSort>() {
         public int compare(final DataToSort d1, final DataToSort d2) {
            return Ordering.natural().
-                  nullsFirst().
                   compare(d1.getIndex(),
                           d2.getIndex());
         }
